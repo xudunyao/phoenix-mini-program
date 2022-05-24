cc4dff17f35161a35b616a937d3311a7d9d8ebb1
周文丽 <wenli.zhou@fujfu.com>
2022-05-24 14:34:21 +0800

    result组件去模块化

--- src/pages/result/index.js
+++ src/pages/result/index.js
@@ -1,17 +1,23 @@
-import { Result } from "@/components";
-import { Button } from '@tarojs/components';
+import { Result, Button } from "@/components";
 import { resultImg } from '@/constants';
 
 const ResultPage = () => {
   const icon = {
     src:resultImg.success
   }
+  const customStyles = `
+    background: linear-gradient(222deg, #80A2FF 0%, #5482FF 100%),
+    box-shadow: 0px 2px 5px 0px rgba(128, 162, 255, 0.35),
+    text-shadow: 0px 2px 5px rgba(128, 162, 255, 0.35);
+    font-weight: 600;
+  `;
+
   return (
     <Result
       icon={icon}
       title='恭喜您，报名成功！'
       subTitle='报名成功后通过筛选会收到面试短信，可自行前往面试地址进行面试'
-      extra={<Button className='parmary'>查看更多岗位</Button>}
+      extra={<Button type='primary' customStyles={customStyles} >查看更多岗位</Button>}
     />
   )
 }
