354ffb345ddd2c938e50510f22be6a83355a72e1
周文丽 <wenli.zhou@fujfu.com>
2022-06-10 19:01:09 +0800

    修改样式和时间

--- src/constants/datetimeFormat.js
+++ src/constants/datetimeFormat.js
@@ -1,8 +1,8 @@
 export default {
-  date: 'YYYY-MM-DD',
+  date: 'YYYY/MM/DD',
   time: 'HH:mm:ss',
   hourMin: 'HH:mm',
-  dateTime: 'YYYY-MM-DD HH:mm:ss',
-  dateHourMin: 'YYYY-MM-DD HH:mm',
-  dateYearMonth: 'YYYY-MM',
+  dateTime: 'YYYY/MM/DD HH:mm:ss',
+  dateHourMin: 'YYYY/MM/DD HH:mm',
+  dateYearMonth: 'YYYY/MM',
 };
