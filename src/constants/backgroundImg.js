10d09e5a1805769fca03aec4609a7c86f6952dd8
boliang.chen <boliang.chen@fujfu.com>
2022-08-01 09:00:05 +0800

    fix: 修复照片地址

--- src/constants/backgroundImg.js
+++ src/constants/backgroundImg.js
@@ -4,4 +4,5 @@ export default {
   invitationBackground: 'https://blue-collar-prod.oss-cn-shenzhen.aliyuncs.com/public/v1.0/userGiftBackground.png',
   activityBackground: 'https://blue-collar-prod.oss-cn-shenzhen.aliyuncs.com/public/v1.0/userGiftBackground1.png',
   inviteRecordBackground: 'https://blue-collar-prod.oss-cn-shenzhen.aliyuncs.com/public/v1.0/inviteRecord.png',
+  introduce: 'https://blue-collar-prod.oss-cn-shenzhen.aliyuncs.com/public/v1.0/%E5%B9%B3%E5%8F%B0%E4%BB%8B%E7%BB%8D.png',
 }
