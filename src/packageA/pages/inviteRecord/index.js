7f886a6b98585da9928192b5db247a26d487fe08
陈柏良 <boliang.chen@fujfu.com>
2022-08-05 20:01:52 +0800

    fix: 修改去提现按钮

--- src/packageA/pages/inviteRecord/index.js
+++ src/packageA/pages/inviteRecord/index.js
@@ -111,8 +111,8 @@ const InviteRecord = () => {
             ))
           }
         </View>
-        <View className={styles.withdrawCash} onClick={handleClick} >去提现</View>
         <View style={{ height: "240px" }}><Items type={type}></Items></View>
+        <View className={styles.withdrawCash} onClick={handleClick} >去提现</View>
       </View>
     </View>
   )
