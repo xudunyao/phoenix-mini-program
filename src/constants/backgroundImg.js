1cee6a5660178a7d06b877ad21673ae7c01d6432
boliang.chen <boliang.chen@fujfu.com>
2022-08-08 10:20:50 +0800

    fix: 修改邀请记录背景颜色

--- src/constants/backgroundImg.js
+++ src/constants/backgroundImg.js
@@ -3,7 +3,7 @@ export default {
   baseImg: 'https://blue-collar-prod.oss-cn-shenzhen.aliyuncs.com/public/v1.0/baseImg.png',
   invitationBackground: 'https://blue-collar-prod.oss-cn-shenzhen.aliyuncs.com/public/v1.0/userGiftBackground.png',
   activityBackground: 'https://blue-collar-prod.oss-cn-shenzhen.aliyuncs.com/public/v1.0/userGiftBackground1.png',
-  inviteRecordBackground: 'https://blue-collar-prod.oss-cn-shenzhen.aliyuncs.com/public/v1.0/inviteRecord.png',
+  inviteRecordBackground: 'https://blue-collar-prod.oss-cn-shenzhen.aliyuncs.com/public/v1.0/inviteRecordBackground.png',
   introduce: 'https://blue-collar-prod.oss-cn-shenzhen.aliyuncs.com/public/v1.0/%E5%B9%B3%E5%8F%B0%E4%BB%8B%E7%BB%8D%402x.png',
   poster: 'https://blue-collar-prod.oss-cn-shenzhen.aliyuncs.com/public/v1.0/%E7%BB%84%2025%402x%20%281%29.png',
 }
