f87a353d4643e0a172bd1ebbba05f7f562d0f3a8
xudunyao <dunyao.xu@fujfu.com>
2022-09-01 15:30:07 +0800

    fix: 修改报名状态

--- src/pages/registration/components/item/index.js
+++ src/pages/registration/components/item/index.js
@@ -5,7 +5,29 @@ import { datetimeFormat } from '@/constants';
 import enroll_success from './img/enroll_success.png';
 import styles from "./Item.modules.scss";
 
-
+const stateText = {
+  JOINED_ON: '在职',
+  JOINED_RESIGED: '已离职',
+  SIGN_UP_PENDING:  '已报名',
+  SIGN_UP_NO_INTENTION: '未通过',
+  SIGN_UP_NO_CONSIDER:'已报名',
+  SIGN_UP_INTENTION: '已报名',
+  INTERVIEW_PENDING: '面试中',
+  INTERVIEW_PASS: '面试中',
+  INTERVIEW_CONSIDER :'面试中',
+  INTERVIEW_FAIL: '未通过',
+  EMPLOYMENT_PENDING: '待入职',
+  EMPLOYMENT_PASS: '待入职',
+  EMPLOYMENT_FAIL: '未通过',
+}
+const stateColor = {
+  "已报名": styles['status_signUP'],
+  "面试中": styles['status_INTERVIEW'],
+  "未通过": styles['status_signUP'],
+  "待入职": styles['status_EMPLOYMENT'],
+  "在职": styles['status_JOINED_ON'],
+  "已离职": styles['tatus_signUP'],
+}
 const Item = (
   {
     data,
@@ -13,35 +35,33 @@ const Item = (
  ) => {
   return (
     <View className={styles.item}>
-        <View className={styles.content}>
-            <View>
-              <View className={styles.title}>{data.jobName}</View>
-              <View className={styles.wrapper}>
-                <View className={styles.wages}>
-                  <Text className={styles.money}>{data.orderPriceStart}{data?.orderPriceEnd && `-${data?.orderPriceEnd}`}</Text><Text>{data?.orderPriceType}</Text>
+        <View>
+          <View className={styles.head}>
+            <View className={styles.title}>{data.jobName}</View>
+            <View className={stateColor[stateText[data.status]]}>{stateText[data.status]}</View>
+          </View>
+          <View className={styles.wrapper}>
+            <View className={styles.wages}>
+              <Text className={styles.money}>{data.orderPriceStart}{data?.orderPriceEnd && `-${data?.orderPriceEnd}`}</Text><Text>{data?.orderPriceType}</Text>
+            </View>
+            {
+              data?.subsidyAmount ? (
+                <View className={styles.subsidy} >
+                  <Text className={styles.money}>{data?.subsidyAmount}</Text> <Text>元/时补贴</Text>
                 </View>
-                {
-                  data?.subsidyAmount ? (
-                    <View className={styles.subsidy} >
-                      <Text className={styles.money}>{data?.subsidyAmount}</Text> <Text>元/时补贴</Text>
-                    </View>
-                  ) : null
-                }
-                
-              </View>
-              <View className={styles.tags}>
-                {
-                  data.tags.slice(0,3).map((item, index) => {
-                    return (
-                      <View className={styles.tag} key={index}>{item}</View>
-                    )
-                  })
-                }
-              </View>
-            </View>
-            <View>
-                 <Image src={enroll_success}  style='width: 82px;height: 75px' />
-            </View>
+              ) : null
+            }
+
+          </View>
+          <View className={styles.tags}>
+            {
+              data.tags.slice(0,3).map((item, index) => {
+                return (
+                  <View className={styles.tag} key={index}>{item}</View>
+                )
+              })
+            }
+          </View>
         </View>
         <View className={styles.root}>
             <View className={`${styles.map} ${styles.center} ${styles['root-text']}`}>
